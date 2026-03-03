73a3a5d5febfddc85ebdacabc68d9ccda65b5f45
Diana Allar <118851223+dianallar@users.noreply.github.com>
2026-03-03 01:29:54 -0500

    Add Chirper

---
+++ firebase-config.example.js
@@ -0,0 +1,10 @@
+// 1) Copy this file to firebase-config.js
+// 2) Replace values with your Firebase Web App config from Firebase Console
+window.CHIRPER_FIREBASE = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.CHIRPER_FIREBASE = {
+  apiKey: "AIzaSyC00aKJ2kPolhviSDxVEpKYu33Q0qme2X0",
+  authDomain: "chirper-ea2bb.firebaseapp.com",
+  projectId: "chirper-ea2bb",
+  storageBucket: "chirper-ea2bb.firebasestorage.app",
+  messagingSenderId: "503480258921",
+  appId: "1:503480258921:web:a81ddcf7c375b694cdadf8",
+  measurementId: "G-F5YZZFQWS1"
+};
